872ab511150f400ad964b47c32938c81d9a9fd7d
baejeu <zeus20240228@gmail.com>
2025-08-26 16:59:41 +0900

    [프로그래머스 고득점KIT]: 스택/큐: 같은 숫자는 싫어

---
+++ programmers2/HighScoreKit/stackqueue/StackAndQueue1.java
@@ -0,0 +1,19 @@
+package programmers2.HighScoreKit.stackqueue;
+
+/**
+ * 스택/큐 /  같은 숫자는 싫어
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12906
+ */
+import java.util.*;
+public class StackAndQueue1 {
+	public int[] solution(int []arr) {
+		Deque<Integer> dq = new LinkedList<>();
+		dq.addLast(arr[0]);
+		for(int i=1; i<arr.length; i++) {
+			if(dq.peekLast() != arr[i]) {
+				dq.addLast(arr[i]);
+			}
+		}
+		return dq.stream().mapToInt(i -> i).toArray();
+	}
+}
